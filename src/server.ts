5f6e4f65fe2bd93bdb5ee8a62ab48389f50b2e9d
Julia Limp <julia.limp@hotmail.com>
2023-08-04 15:36:56 -0300

    Configurações Express

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,12 @@
-const http = require("http");
+import express from "express";
 
-const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/plain");
+const port = 3000;
+const app = express();
 
-    if (req.url === "/") {
-        res.statusCode = 200;
-        res.end("Welcome to Home Page");
-    } else if (req.url === "/about") {
-        res.statusCode = 200;
-        res.end("About Page");
-    } else {
-        res.statusCode = 404;
-        res.end("Page not found");
-    }
+app.get("/movies", (req, res) => {
+    res.send("Listagem de filmes");
 });
 
-server.listen(3000, () => {
-    console.log("Server listening on port 3000 at http://localhost:3000");
+app.listen(3000, () => {
+    console.log(`Server listening on port ${port}`);
 });
-
