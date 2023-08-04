8f4583bd245a5cb1b548abed36362832be68ebb3
Julia Limp <julia.limp@hotmail.com>
2023-08-04 15:20:00 -0300

    configurações ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
-const http = require('http')
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader("Content-Type", "text/plain");
 
-    if (req.url === '/') {
+    if (req.url === "/") {
         res.statusCode = 200;
-        res.end('Welcome to Home Page')
-    } else if (req.url === '/about') {
+        res.end("Welcome to Home Page");
+    } else if (req.url === "/about") {
         res.statusCode = 200;
-        res.end('About Page')
+        res.end("About Page");
     } else {
         res.statusCode = 404;
-        res.end('Page not found')
+        res.end("Page not found");
     }
-})
+});
 
 server.listen(3000, () => {
-    console.log('Server listening on port 3000 at http://localhost:3000');
-})
+    console.log("Server listening on port 3000 at http://localhost:3000");
+});
+
